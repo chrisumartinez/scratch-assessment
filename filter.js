7f56fea57a3bf883e3793bb3b9da80776090dcbe
chrisumartinez <chrisumartinez@gmail.com>
2021-11-22 03:18:13 -0800

    filter.js: - Added Logic to filter json by State, Name of Clinic, and Availability - flexible enough to filter for only dental clinic and vet clinic - Uses states.js to filter for State Abbreviation ("CA", "KS")
    index.js:
    - Clustered Logic to fetch data from scratch api json
    - Hardcoded url's for fetch data
    - Chooses Filter depending on process.argv[2] ('filterByName', 'filterByState')
    
    TODO:
    Incorporate logic to filter results by multiple filters (filterByName + filterByState)
    Test Logic using the Jest Framework
    Restructure Codebase
    
    EXTRA:
    Dockerize the project
    Integrate CircleCI for continuous integration

---
+++ filter.js
@@ -0,0 +1,64 @@
+const data = require("./dental-clinics.json");
+const states = require("./states.js");
+
+const filterByName = (json, name) => {
+	return json.filter((item) => {
+		if (item.clinicName) {
+			return item.clinicName.toUpperCase().includes(name.toUpperCase());
+		} else {
+			return item.name.toUpperCase().includes(name.toUpperCase());
+		}
+	});
+};
+
+const filterByState = (json, state) => {
+	if (state.length === 2) {
+		if (state in states.states) {
+			strState = states.states[state];
+			return json.filter((item) => {
+				if (item.stateName === strState) {
+					return true;
+				}
+			});
+		}
+	} else {
+		return json.filter((item) => {
+			if (item.stateName === state) {
+				return true;
+			}
+		});
+	}
+};
+
+const filterByTime = (json, time) => {
+	const timeToDecimal = (time) => {
+		let arr = time.split(":");
+		let dec = parseInt((arr[1] / 6) * 10, 10);
+
+		return parseFloat(
+			parseInt(arr[0], 10) + "." + (dec < 10 ? "0" : "") + dec
+		);
+	};
+
+	return json.filter((item) => {
+		if (item.availability) {
+			if (
+				timeToDecimal(item.availability.from) <= timeToDecimal(time) &&
+				timeToDecimal(time) <= timeToDecimal(item.availability.to)
+			) {
+				return true;
+			}
+		} else {
+			if (
+				timeToDecimal(item.opening.from) <= timeToDecimal(time) &&
+				timeToDecimal(time) <= timeToDecimal(item.opening.to)
+			) {
+				return true;
+			}
+		}
+	});
+};
+
+module.exports.filterByName = filterByName;
+module.exports.filterByState = filterByState;
+module.exports.filterByTime = filterByTime;
---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const fs = require("fs");
+const axios = require("axios");
+const res = require("express/lib/response");
+const filters = require("./filter.js");
+const { all } = require("express/lib/application");
+
+const medical_url =
+	"https://storage.googleapis.com/scratchpay-code-challenge/dental-clinics.json";
+const vet_url =
+	"https://storage.googleapis.com/scratchpay-code-challenge/vet-clinics.json";
+
+const getMedicalClinic = () => {
+	return axios.get(medical_url);
+};
+
+const getVetClinic = () => {
+	return axios.get(vet_url);
+};
+
+const returnFilteredClinics = (json, args) => {
+	if (args[0] === "filterByName") {
+		return filters.filterByName(json, args[1]);
+	}
+	if (args[0] === "filterByState") {
+		return filters.filterByState(json, args[1]);
+	}
+	if (args[0] === "filterByTime") {
+		return filters.filterByTime(json, args[1]);
+	}
+};
+
+app.get("/", (req, res) => {
+	const getAllClinics = async () => {
+		return await axios.all([getMedicalClinic(), getVetClinic()]).then(
+			axios.spread((...responses) => {
+				const medResponse = responses[0].data;
+				const vetResponse = responses[1].data;
+				const clinics = medResponse.concat(vetResponse);
+				const args = process.argv.slice(2);
+				const filteredClinics = returnFilteredClinics(clinics, args);
+				res.send(JSON.stringify(filteredClinics, null, 4));
+			})
+		);
+	};
+	getAllClinics();
+});
+
+app.listen(port, () => {
+	console.log(`app listening at http://localhost:${port}`);
+});
